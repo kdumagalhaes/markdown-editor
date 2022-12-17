a48ae0a1e8f21a4dfbeaf44993f096f9c76fbcfc
Kadu <ce.magalhaespereira@gmail.com>
2022-12-17 16:25:52 -0300

    Add components

---
+++ src/components/RawInputArea/index.tsx
@@ -0,0 +1,8 @@
+export function RawInputArea() {
+    return (
+        <div>
+        <h1 className="title">Enter Markdown</h1>
+        <textarea className="raw-area" name="raw-area" id="" cols={30} rows={10}></textarea>
+        </div>
+    )
+}
