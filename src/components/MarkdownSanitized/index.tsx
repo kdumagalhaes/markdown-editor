8735e5a7eb561c94b23d881f1d38bcc33150a681
Kadu <ce.magalhaespereira@gmail.com>
2022-12-17 22:27:16 -0300

    Add styles

--- src/components/MarkdownSanitized/index.tsx
+++ src/components/MarkdownSanitized/index.tsx
@@ -1,14 +1,15 @@
 import {useEditor} from '../../hooks/useEditor'
 import parse from 'html-react-parser'
+import styles from './styles.module.css'
 
 export function MarkdownSanitized() {
     const {getMarkDownAsHTMLOutput} = useEditor()
     const output = parse(getMarkDownAsHTMLOutput())
     
     return (
-        <div>
-        <h2 className="title">Markdown Output</h2>
-        <div className="markdown-output">{output}</div>
+        <div className={styles.container}>
+        <h2 className={styles.title}>Markdown Output</h2>
+        <div className={styles['markdown-output']}>{output}</div>
         </div>
     )
 }
